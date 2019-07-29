864ca13bd8e090d841603f168908c337ea5bee57
dan_mcmanus <danmcmanus11@outlook.com>
2019-07-28 20:24:51 -0500

    removing unused references, code clean-up

--- api/clues.js
+++ api/clues.js
@@ -1,33 +1,21 @@
 const rp = require('request-promise');
-const _ = require('lodash');
+let cluesByCategoryResponse = [],
+    allUrls = [];
 const baseApiUri = 'http://jservice.io/api';
+const count = 50;
 
-// const singleJeopardyValues = [100, 200, 300, 400, 500];
-// const doubleJeopardyValues = singleJeopardyValues.map(val => val * 2);
-let cluesByCategoryResponse = [];
-let grouped = [];
-let categoryIds = [];
-let allUrls = [];
-
-const categoryOptions ={
-    uri: `${baseApiUri}/categories?count=50`,
+const categoryOptions = {
+    uri: `${baseApiUri}/categories?count=${count}`,
     json: true
 };
 
 const categoriesPromise = rp(categoryOptions);
 
 categoriesPromise.then((response) => {
-
-    const rand1 = random(1, 50);
-    const rand2 = random(1, 50);
-    const rand3 = random(1, 50);
-    const rand4 = random(1, 50);
-    const rand5 = random(1, 50);
-
-    const randomIndexes = [rand1, rand2, rand3, rand4, rand5];
+    const randomIndexes = [random(1, count), random(1, count), random(1, count), random(1, count), random(1, count)];
 
     randomIndexes.forEach((idx) => {
-        urls = getUrls(response[idx].id); 
+        urls = getUrls(response[idx].id);
         urls.forEach(url => allUrls.push(url));
     });
 
@@ -38,13 +26,13 @@ categoriesPromise.then((response) => {
 }).then((urls) => {
     let promises = [];
     urls.forEach((url) => {
-        let promise = rp({uri: url, json: true});
+        let promise = rp({ uri: url, json: true });
         promises.push(promise);
     });
 
     Promise.all(promises).then((response) => {
         response.forEach((val) => {
-            if (val.length > 0){
+            if (val.length > 0) {
                 const clue = {
                     categoryId: val[0].category.id,
                     category: val[0].category.title,
@@ -57,14 +45,11 @@ categoriesPromise.then((response) => {
             }
         })
     });
-    
+
 });
 
-
-
-
 function getUrls(categoryId) {
-    const urls = [
+    return [
         `${baseApiUri}/clues?value=100&category=${categoryId}`,
         `${baseApiUri}/clues?value=200&category=${categoryId}`,
         `${baseApiUri}/clues?value=300&category=${categoryId}`,
@@ -74,10 +59,8 @@ function getUrls(categoryId) {
         `${baseApiUri}/clues?value=700&category=${categoryId}`,
         `${baseApiUri}/clues?value=800&category=${categoryId}`,
         `${baseApiUri}/clues?value=900&category=${categoryId}`,
-        `${baseApiUri}/clues?value=1000&category=${categoryId}`,
+        `${baseApiUri}/clues?value=1000&category=${categoryId}`
     ];
-    categoryIds.push(categoryId);
-    return urls;
 }
 
 function random(min, max) {
@@ -87,4 +70,5 @@ function random(min, max) {
 const data = {
     boardData: cluesByCategoryResponse
 }
+
 module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const api = require('../api/clues');
 const _ = require('lodash');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
   const boardData = api.boardData;
   let categories = [];
